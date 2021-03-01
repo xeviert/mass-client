61b4ed50bd03f45e6700d3c62294ddb5221b2f84
Xevier Turrubiartes <xevierturrubiartes@gmail.com>
2021-03-01 14:25:42 -0600

    deployed to vercel

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
-export default {
+const config = {
   API_ENDPOINT: "https://pacific-shelf-78351.herokuapp.com/api",
   TOKEN_KEY: "mutual-aid-shared-support-auth-token",
 };
+
+export default config
