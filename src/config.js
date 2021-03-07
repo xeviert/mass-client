d88dac98f40a2bcad6badf37d3cd7e3fb81f89b0
Xevier Turrubiartes <xevierturrubiartes@gmail.com>
2021-03-07 02:19:05 -0600

    re-dploy after more tests

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
-  API_ENDPOINT: "http://localhost:8000/api",
+  API_ENDPOINT: "https://pacific-shelf-78351.herokuapp.com/api",
   TOKEN_KEY: "mutual-aid-shared-support-auth-token",
 };
-// https://pacific-shelf-78351.herokuapp.com/api
+
 export default config
