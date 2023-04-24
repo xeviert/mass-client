97ce7410682f813435914e2c721b7b161189bd31
xeviert <xevierturrubiartes@gmail.com>
2023-04-25 01:10:03 +0700

    change font and home page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -74,6 +74,58 @@ export default class Home extends Component {
     this.setState({ location });
   };
 
+  renderItemList = () => {
+    const items = [
+      {id: 1, 'name': 'Snack Kit'},
+      {id: 2, 'name': 'Socks and Underwear'},
+      {id: 3, 'name': 'Walking Shoes'},
+      {id: 4, 'name': 'Pads/Tampons'},
+      {id: 5, 'name': 'First Aid Kit'},
+      {id: 6, 'name': 'Dental Care Kit'},
+      {id: 7, 'name': 'Deodorant and Soap'},
+      {id: 8, 'name': 'Earplugs'},
+      {id: 9, 'name': 'Face Mask, Sanitizer, Gloves'},
+      {id: 10, 'name': 'Blanket'},
+      {id: 11, 'name': 'Diapers, Wipes, Baby Clothes'},
+      {id: 12, 'name': 'Baby Formula'},
+      {id: 13, 'name': 'School Supplies'},
+      {id: 14, 'name': 'Notepad and Pens'},
+      {id: 15, 'name': 'Hat, Mittens, Scarf'},
+      {id: 16, 'name': 'Sweater/Jacket'},
+      {id: 17, 'name': 'Narcan/Naloxone'}
+    ]
+
+    return (
+      <div>
+        {items.map((item) => (
+            <div className='item'>
+              <label className='item-label'>
+                <input
+                  type='checkbox'
+                  className='checkbox-btn'
+                  value={item.id}
+                  onChange={() => this.toggleItemInList(item.id)}
+                />
+                <div className='item-text'>{item.name}</div>
+              </label>
+
+              <label className='quantity-label-and-input'>
+                <div className='quantity-label'>Quantity</div>
+                <input
+                  type='number'
+                  className='quantity'
+                  min='0'
+                  max='20'
+                  onChange={(e) => this.updateQuantityOfItem(item.id, e.target.value)}
+                  />
+              </label>
+            </div>
+        ))}
+      </div>
+
+    )
+  }
+
   render() {
     return (
       <div>
@@ -92,375 +144,8 @@ export default class Home extends Component {
                   required
                 />
               </label>
-
-            <div className='item'>
-              <label className='item-label'>
-                <input
-                  type='checkbox'
-                  className='checkbox-btn'
-                  value='1'
-                  onChange={() => this.toggleItemInList(1)}
-                />
-                <div className='item-text'>Snack Kit</div>
-              </label>
-
-              <label className='quantity-label-and-input'>
-                <div className='quantity-label'>Quantity</div>
-                <input
-                  type='number'
-                  className='quantity'
-                  min='0'
-                  max='20'
-                  onChange={(e) => this.updateQuantityOfItem(1, e.target.value)}
-                  />
-              </label>
-            </div>
-
-              <div className='item'>
-              <label className='item-label'>
-                <input
-                  type='checkbox'
-                  className='checkbox-btn'
-                  value='2'
-                  onChange={() => this.toggleItemInList(2)}
-                />
-                <div className='item-text'>Socks and Underwear</div>
-                </label>
-
-                <label>
-                <div className='quantity-label'>Quantity</div>
-                <input
-                  type='number'
-                  className='quantity'
-                  min='0'
-                  max='20'
-                  onChange={(e) => this.updateQuantityOfItem(2, e.target.value)}
-                />
-              </label>
-              </div>
-
-              <div className='item'>
-              <label className='item-label'>
-                <input
-                  type='checkbox'
-                  className='checkbox-btn'
-                  value='3'
-                  onChange={() => this.toggleItemInList(3)}
-                />
-                <div className='item-text'>Walking Shoes</div>
-                </label>
-                <label>
-                <div className='quantity-label'>Quantity</div>
-                <input
-                  type='number'
-                  className='quantity'
-                  min='0'
-                  max='20'
-                  onChange={(e) => this.updateQuantityOfItem(3, e.target.value)}
-                />
-              </label>
-              </div>
-
-              <div className='item'>
-              <label className='item-label'>
-                <input
-                  type='checkbox'
-                  className='checkbox-btn'
-                  value='4'
-                  onChange={() => this.toggleItemInList(4)}
-                />
-                <div className='item-text'>Pads/Tampons</div>
-                </label>
-                <label>
-                <div className='quantity-label'>Quantity</div>
-                <input
-                  type='number'
-                  className='quantity'
-                  min='0'
-                  max='20'
-                  onChange={(e) => this.updateQuantityOfItem(4, e.target.value)}
-                />
-              </label>
-              </div>
-
-              <div className='item'>
-              <label className='item-label'>
-                <input
-                  type='checkbox'
-                  className='checkbox-btn'
-                  value='5'
-                  onChange={() => this.toggleItemInList(5)}
-                />
-                <div className='item-text'>First Aid Kit</div>
-                </label>
-                <label>
-                <div className='quantity-label'>Quantity</div>
-                <input
-                  type='number'
-                  className='quantity'
-                  min='0'
-                  max='20'
-                  onChange={(e) => this.updateQuantityOfItem(5, e.target.value)}
-                />
-              </label>
-              </div>
-
-              <div className='item'>
-              <label className='item-label'>
-                <input
-                  type='checkbox'
-                  className='checkbox-btn'
-                  value='6'
-                  onChange={() => this.toggleItemInList(6)}
-                />
-                <div className='item-text'>Dental Care Kit</div>
-                </label>
-                <label>
-                <div className='quantity-label'>Quantity</div>
-                <input
-                  type='number'
-                  className='quantity'
-                  min='0'
-                  max='20'
-                  onChange={(e) => this.updateQuantityOfItem(6, e.target.value)}
-                />
-              </label>
-              </div>
               
-
-              <div className='item'>
-              <label className='item-label'>
-                <input
-                  type='checkbox'
-                  className='checkbox-btn'
-                  value='7'
-                  onChange={() => this.toggleItemInList(7)}
-                />
-                <div className='item-text'>Deodorant and Soap</div>
-                </label>
-                <label>
-                <div className='quantity-label'>Quantity</div>
-                <input
-                  type='number'
-                  className='quantity'
-                  min='0'
-                  max='20'
-                  onChange={(e) => this.updateQuantityOfItem(7, e.target.value)}
-                />
-              </label>
-              </div>
-
-              <div className='item'>
-              <label className='item-label'>
-                <input
-                  type='checkbox'
-                  className='checkbox-btn'
-                  value='8'
-                  onChange={() => this.toggleItemInList(8)}
-                />
-                <div className='item-text'>Earplugs</div>
-                </label>
-                <label>
-                <div className='quantity-label'>Quantity</div>
-                <input
-                  type='number'
-                  className='quantity'
-                  min='0'
-                  max='20'
-                  onChange={(e) => this.updateQuantityOfItem(8, e.target.value)}
-                />
-              </label>
-              </div>
-
-              <div className='item'>
-              <label className='item-label'>
-                <input
-                  type='checkbox'
-                  className='checkbox-btn'
-                  value='9'
-                  onChange={() => this.toggleItemInList(9)}
-                />
-                <div className='item-text'>Face Mask, Sanitizer, Gloves</div>
-                </label>
-                <label>
-                <div className='quantity-label'>Quantity</div>
-                <input
-                  type='number'
-                  className='quantity'
-                  min='0'
-                  max='20'
-                  onChange={(e) => this.updateQuantityOfItem(9, e.target.value)}
-                />
-              </label>
-              </div>
-
-              <div className='item'>
-              <label className='item-label'>
-                <input
-                  type='checkbox'
-                  className='checkbox-btn'
-                  value='10'
-                  onChange={() => this.toggleItemInList(10)}
-                />
-                <div className='item-text'>Blanket</div>
-                </label>
-                <label>
-                <div className='quantity-label'>Quantity</div>
-                <input
-                  type='number'
-                  className='quantity'
-                  min='0'
-                  max='20'
-                  onChange={(e) =>
-                    this.updateQuantityOfItem(10, e.target.value)
-                  }
-                />
-              </label>
-              </div>
-
-              <div className='item'>
-              <label className='item-label'>
-                <input
-                  type='checkbox'
-                  className='checkbox-btn'
-                  value='11'
-                  onChange={() => this.toggleItemInList(11)}
-                />
-                <div className='item-text'>Diapers, Wipes, Baby Clothes</div>
-                </label>
-                <label>
-                <div className='quantity-label'>Quantity</div>
-                <input
-                  type='number'
-                  className='quantity'
-                  min='0'
-                  max='20'
-                  onChange={(e) =>
-                    this.updateQuantityOfItem(11, e.target.value)
-                  }
-                />
-              </label>
-              </div>
-
-              <div className='item'>
-              <label className='item-label'>
-                <input
-                  type='checkbox'
-                  className='checkbox-btn'
-                  value='12'
-                  onChange={() => this.toggleItemInList(12)}
-                />
-                <div className='item-text'>Baby Formula</div>
-                </label>
-                <label>
-                <div className='quantity-label'>Quantity</div>
-                <input
-                  type='number'
-                  className='quantity'
-                  min='0'
-                  max='20'
-                  onChange={(e) =>
-                    this.updateQuantityOfItem(12, e.target.value)
-                  }
-                />
-              </label>
-              </div>
-
-              <div className='item'>
-              <label className='item-label'>
-                <input
-                  type='checkbox'
-                  className='checkbox-btn'
-                  value='13'
-                  onChange={() => this.toggleItemInList(13)}
-                />
-                <div className='item-text'>School Supplies</div>
-                </label>
-                <label>
-                <div className='quantity-label'>Quantity</div>
-                <input
-                  type='number'
-                  className='quantity'
-                  min='0'
-                  max='20'
-                  onChange={(e) =>
-                    this.updateQuantityOfItem(13, e.target.value)
-                  }
-                />
-              </label>
-              </div>
-
-              <div className='item'>
-              <label className='item-label'>
-                <input
-                  type='checkbox'
-                  className='checkbox-btn'
-                  value='14'
-                  onChange={() => this.toggleItemInList(14)}
-                />
-                <div className='item-text'>Notepad and Pens</div>
-                </label>
-                <label>
-                <div className='quantity-label'>Quantity</div>
-                <input
-                  type='number'
-                  className='quantity'
-                  min='0'
-                  max='20'
-                  onChange={(e) =>
-                    this.updateQuantityOfItem(14, e.target.value)
-                  }
-                />
-              </label>
-              </div>
-
-              <div className='item'>
-              <label className='item-label'>
-                <input
-                  type='checkbox'
-                  className='checkbox-btn'
-                  value='15'
-                  onChange={() => this.toggleItemInList(15)}
-                />
-                <div className='item-text'>Hat, Mittens, Scarf</div>
-                </label>
-                <label>
-                <div className='quantity-label'>Quantity</div>
-                <input
-                  type='number'
-                  className='quantity'
-                  min='0'
-                  max='20'
-                  onChange={(e) =>
-                    this.updateQuantityOfItem(15, e.target.value)
-                  }
-                />
-              </label>
-              </div>
-
-              <div className='item'>
-              <label className='item-label'>
-                <input
-                  type='checkbox'
-                  className='checkbox-btn'
-                  value='16'
-                  onChange={() => this.toggleItemInList(16)}
-                />
-                <div className='item-text'>Sweater/Jacket</div>
-                </label>
-                <label>
-                <div className='quantity-label'>Quantity</div>
-                <input
-                  type='number'
-                  className='quantity'
-                  min='0'
-                  max='20'
-                  onChange={(e) =>
-                    this.updateQuantityOfItem(16, e.target.value)
-                  }
-                />
-              </label>
-              </div>
+              {this.renderItemList()}
 
               <button type='submit' id='wishlist-sbt-btn'>
                 Submit List
