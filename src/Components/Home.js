0cfcdc962e8361277d98c4188a521f85bedc92b8
xeviert <xevierturrubiartes@gmail.com>
2024-06-08 00:55:29 +0200

    updated checklist logic

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -6,6 +6,7 @@ import "./Styling/Home.css";
 export default class Home extends Component {
   state = {
     order_items: {},
+    checked_items: {},
     location: "",
   };
 
@@ -16,22 +17,27 @@ export default class Home extends Component {
   }
 
   toggleItemInList = (itemId) => {
-    const { order_items } = this.state;
+    const { order_items, checked_items } = this.state;
 
-    if (order_items[itemId]) {
+    if (checked_items[itemId]) {
       delete order_items[itemId];
-      this.setState({ order_items });
+      delete checked_items[itemId];
+      this.setState({ order_items, checked_items });
     } else {
       this.setState({
-        order_items: { ...this.state.order_items, [itemId]: 0 },
+        checked_items: { ...checked_items, [itemId]: true },
+        order_items: { ...order_items, [itemId]: 0 },
       });
     }
   };
 
   updateQuantityOfItem = (itemId, quantity) => {
-    this.setState({
-      order_items: { ...this.state.order_items, [itemId]: quantity },
-    });
+    const parsedQuantity = parseInt(quantity, 10);
+    if (!isNaN(parsedQuantity)) {
+      this.setState({
+        order_items: { ...this.state.order_items, [itemId]: parsedQuantity },
+      });
+    }
   };
 
   handleSubmit = (e) => {
@@ -57,6 +63,7 @@ export default class Home extends Component {
         this.setState({
           location: "",
           order_items: {},
+          checked_items: {},
         });
         this.resetForm();
       })
@@ -76,70 +83,73 @@ export default class Home extends Component {
 
   renderItemList = () => {
     const items = [
-      { id: 1, 'name': 'Snack Kit' },
-      { id: 2, 'name': 'Socks and Underwear' },
-      { id: 3, 'name': 'Walking Shoes' },
-      { id: 4, 'name': 'Pads/Tampons' },
-      { id: 5, 'name': 'First Aid Kit' },
-      { id: 6, 'name': 'Dental Care Kit' },
-      { id: 7, 'name': 'Deodorant and Soap' },
-      { id: 8, 'name': 'Earplugs' },
-      { id: 9, 'name': 'Face Mask, Sanitizer, Gloves' },
-      { id: 10, 'name': 'Blanket' },
-      { id: 11, 'name': 'Diapers, Wipes, Baby Clothes' },
-      { id: 12, 'name': 'Baby Formula' },
-      { id: 13, 'name': 'School Supplies' },
-      { id: 14, 'name': 'Notepad and Pens' },
-      { id: 15, 'name': 'Hat, Mittens, Scarf' },
-      { id: 16, 'name': 'Sweater/Jacket' },
-      { id: 17, 'name': 'Narcan/Naloxone' }
-    ]
+      { id: 1, name: "Snack Kit" },
+      { id: 2, name: "Socks and Underwear" },
+      { id: 3, name: "Walking Shoes" },
+      { id: 4, name: "Pads/Tampons" },
+      { id: 5, name: "First Aid Kit" },
+      { id: 6, name: "Dental Care Kit" },
+      { id: 7, name: "Deodorant and Soap" },
+      { id: 8, name: "Earplugs" },
+      { id: 9, name: "Face Mask, Sanitizer, Gloves" },
+      { id: 10, name: "Blanket" },
+      { id: 11, name: "Diapers, Wipes, Baby Clothes" },
+      { id: 12, name: "Baby Formula" },
+      { id: 13, name: "School Supplies" },
+      { id: 14, name: "Notepad and Pens" },
+      { id: 15, name: "Hat, Mittens, Scarf" },
+      { id: 16, name: "Sweater/Jacket" },
+      { id: 17, name: "Narcan/Naloxone" },
+    ];
 
     return (
       <div>
         {items.map((item) => (
-          <div className='item'>
-            <label className='item-label'>
+          <div className="item" key={item.id}>
+            <label className="item-label">
               <input
-                type='checkbox'
-                className='checkbox-btn'
+                type="checkbox"
+                className="checkbox-btn"
                 value={item.id}
                 onChange={() => this.toggleItemInList(item.id)}
               />
-              <div className='item-text'>{item.name}</div>
+              <div className="item-text">{item.name}</div>
             </label>
 
-            <label className='quantity-label-and-input'>
-              <div className='quantity-label'>Quantity</div>
+            <label className="quantity-label-and-input">
+              <div className="quantity-label">Quantity</div>
               <input
-                type='number'
-                className='quantity'
-                min='0'
-                max='20'
-                onChange={(e) => this.updateQuantityOfItem(item.id, e.target.value)}
+                type="number"
+                className="quantity"
+                min="0"
+                max="20"
+                disabled={!this.state.checked_items[item.id]}
+                value={this.state.order_items[item.id] || ""}
+                onChange={(e) =>
+                  this.updateQuantityOfItem(item.id, e.target.value)
+                }
               />
             </label>
           </div>
         ))}
       </div>
-
-    )
-  }
+    );
+  };
 
   render() {
     return (
       <div>
         <div>
-          <div id='home-body'>
+          <div id="home-body">
             <h2>Wishlist</h2>
-            <form onSubmit={(e) => this.handleSubmit(e)} id='wishlist-form'>
-              <label id='location'>
+            <form onSubmit={(e) => this.handleSubmit(e)} id="wishlist-form">
+              <label id="location">
                 Location/Address:
                 <br />
                 <input
-                  type='text'
-                  maxLength='50'
-                  id='location-input'
+                  type="text"
+                  maxLength="50"
+                  id="location-input"
                   onChange={(e) => this.setLocationOnChange(e.target.value)}
                   required
                 />
@@ -147,7 +157,7 @@ export default class Home extends Component {
 
               {this.renderItemList()}
 
-              <button type='submit' id='wishlist-sbt-btn'>
+              <button type="submit" id="wishlist-sbt-btn">
                 Submit List
               </button>
             </form>
