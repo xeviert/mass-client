50f119764d7da3e7fe083a3c4f66a3f16b21cf2d
xeviert <xevierturrubiartes@gmail.com>
2024-06-08 02:15:15 +0200

    more styling for landing and resources

--- src/Components/Resources.js
+++ src/Components/Resources.js
@@ -1,177 +1,100 @@
 import React, { Component } from "react";
-import "./Styling/Resources.css";
+import { Container, Box, Typography, Link } from "@mui/material";
 
 export default class Resources extends Component {
   render() {
     return (
-      <div>
-        <div className='page-body-resources'>
-          <h2>Shared Resources</h2>
-          <p>
-            Our community-sourced, volunteer-curated library is a growing
-            collection of the many resources available to Texans. We are
-            committed to building a comprehensive list of high-quality
-            resources. Check back frequently as new resources are added every
-            day!
-          </p>
+      <Container>
+        <Box className='page-body-resources' sx={{ width: '100%', maxWidth: '900px', fontSize: 'clamp(12px, 16px, 20px)', margin: '0 auto', marginBottom: '10vh' }}>
+          <Typography variant="h2" sx={{ padding: '0 2vh', color: '#12806b', fontWeight: 500 }}>Shared Resources</Typography>
+          <Typography variant="body1" sx={{ marginBottom: 2 }}>
+            Our community-sourced, volunteer-curated library is a growing collection of the many resources available to Texans. We are committed to building a comprehensive list of high-quality resources. Check back frequently as new resources are added every day!
+          </Typography>
 
-          <div className='resource-sections'>
-            <h3>Other Mutual Aid Programs</h3>
-            <p>
-              If we are not able to meet your needs or aren't located near us,
-              please check out one of these mutual aid organizations that we
-              have worked with in the past.
-            </p>
-            <a
-              href='https://ntxmutualaid.org/'
-              rel='noreferrer noopener'
-              target='_blank'
-            >
+          <Box className='resource-sections' sx={{ wordWrap: 'break-word', marginBottom: 4 }}>
+            <Typography variant="h4">Other Mutual Aid Programs</Typography>
+            <Typography variant="body1" sx={{ marginBottom: 2 }}>
+              If we are not able to meet your needs or aren't located near us, please check out one of these mutual aid organizations that we have worked with in the past.
+            </Typography>
+            <Link href='https://ntxmutualaid.org/' rel='noreferrer noopener' target='_blank' sx={{ display: 'block', marginBottom: 1 }}>
               North Texas Mutual Aid
-            </a>{" "}
-            <br />
-            <a
-              href='https://www.mutualaidhou.com/'
-              rel='noreferrer noopener'
-              target='_blank'
-            >
+            </Link>
+            <Link href='https://www.mutualaidhou.com/' rel='noreferrer noopener' target='_blank' sx={{ display: 'block', marginBottom: 1 }}>
               Mutual Aid Houston
-            </a>{" "}
-            <br />
-            <a
-              href='https://linktr.ee/austinmutualaid'
-              rel='noreferrer noopener'
-              target='_blank'
-            >
+            </Link>
+            <Link href='https://linktr.ee/austinmutualaid' rel='noreferrer noopener' target='_blank' sx={{ display: 'block', marginBottom: 1 }}>
               Austin Mutual Aid
-            </a>
-          </div>
-          <div className='resource-sections'>
-            <h3>Shelters</h3>
-            <a
-              href='https://www.shelterlist.com/city/tx-houston'
-              rel='noreferrer noopener'
-              target='_blank'
-            >
+            </Link>
+          </Box>
+
+          <Box className='resource-sections' sx={{ wordWrap: 'break-word', marginBottom: 4 }}>
+            <Typography variant="h4">Shelters</Typography>
+            <Link href='https://www.shelterlist.com/city/tx-houston' rel='noreferrer noopener' target='_blank' sx={{ display: 'block', marginBottom: 1 }}>
               Directory of Houston Shelters
-            </a>{" "}
-            <br />
-            <a
-              href='https://www.shelterlist.com/city/tx-austin'
-              rel='noreferrer noopener'
-              target='_blank'
-            >
+            </Link>
+            <Link href='https://www.shelterlist.com/city/tx-austin' rel='noreferrer noopener' target='_blank' sx={{ display: 'block', marginBottom: 1 }}>
               Directory of Austin Shelters
-            </a>
-          </div>
-          <div className='resource-sections'>
-            <h3>Food Bank</h3>
-            <a
-              href='https://www.houstonfoodbank.org/'
-              rel='noreferrer noopener'
-              target='_blank'
-            >
+            </Link>
+          </Box>
+
+          <Box className='resource-sections' sx={{ wordWrap: 'break-word', marginBottom: 4 }}>
+            <Typography variant="h4">Food Bank</Typography>
+            <Link href='https://www.houstonfoodbank.org/' rel='noreferrer noopener' target='_blank' sx={{ display: 'block', marginBottom: 1 }}>
               Houston Food Bank
-            </a>{" "}
-            <br />
-            <a
-              href='https://www.centraltexasfoodbank.org/'
-              rel='noreferrer noopener'
-              target='_blank'
-            >
+            </Link>
+            <Link href='https://www.centraltexasfoodbank.org/' rel='noreferrer noopener' target='_blank' sx={{ display: 'block', marginBottom: 1 }}>
               Austin Food Bank
-            </a>
-          </div>
-          <div className='resource-sections'>
-            <h3>Community Fridge</h3>
-            <p>Community fridge where you take what you need.</p>
-            <a
-              href='https://houstonfridges.cargo.site/'
-              rel='noreferrer noopener'
-              target='_blank'
-            >
+            </Link>
+          </Box>
+
+          <Box className='resource-sections' sx={{ wordWrap: 'break-word', marginBottom: 4 }}>
+            <Typography variant="h4">Community Fridge</Typography>
+            <Typography variant="body1" sx={{ marginBottom: 2 }}>Community fridge where you take what you need.</Typography>
+            <Link href='https://houstonfridges.cargo.site/' rel='noreferrer noopener' target='_blank' sx={{ display: 'block', marginBottom: 1 }}>
               Houston Community Fridge
-            </a>
-            <br />
-            <a
-              href='https://linktr.ee/atxfreefridge/'
-              rel='noreferrer noopener'
-              target='_blank'
-            >
+            </Link>
+            <Link href='https://linktr.ee/atxfreefridge/' rel='noreferrer noopener' target='_blank' sx={{ display: 'block', marginBottom: 1 }}>
               Austin Community Fridge
-            </a>
-          </div>
-          <div className='resource-sections'>
-            <h3>Domestic Violence</h3>
-            <p>
-              If you are dealing with domestic abuse, you may find safe shelter
-              with one of these organizations.
-            </p>
-            <a
-              href='https://housingandcommunityresources.net/emergency-assistance/domestic-abuse/'
-              rel='noreferrer noopener'
-              target='_blank'
-            >
+            </Link>
+          </Box>
+
+          <Box className='resource-sections' sx={{ wordWrap: 'break-word', marginBottom: 4 }}>
+            <Typography variant="h4">Domestic Violence</Typography>
+            <Typography variant="body1" sx={{ marginBottom: 2 }}>
+              If you are dealing with domestic abuse, you may find safe shelter with one of these organizations.
+            </Typography>
+            <Link href='https://housingandcommunityresources.net/emergency-assistance/domestic-abuse/' rel='noreferrer noopener' target='_blank' sx={{ display: 'block', marginBottom: 1 }}>
               Houston: Housing and Community Resource Center
-            </a>
-            <br />
-            <a
-              href='https://www.safeaustin.org/get-help/'
-              rel='noreferrer noopener'
-              target='_blank'
-            >
+            </Link>
+            <Link href='https://www.safeaustin.org/get-help/' rel='noreferrer noopener' target='_blank' sx={{ display: 'block', marginBottom: 1 }}>
               Austin: The SAFE Alliance
-            </a>
-            <br />
-            <a
-              href='https://www.domesticshelters.org/help/tx/austin'
-              rel='noreferrer noopener'
-              target='_blank'
-            >
+            </Link>
+            <Link href='https://www.domesticshelters.org/help/tx/austin' rel='noreferrer noopener' target='_blank' sx={{ display: 'block', marginBottom: 1 }}>
               Austin: Domestic Shelters
-            </a>
-            <br />
-          </div>
-          <div className='resource-sections'>
-            <h3>Substance Abuse</h3>
-            <a
-              href='https://www.freerehabcenters.org/city/tx-houston'
-              rel='noreferrer noopener'
-              target='_blank'
-            >
+            </Link>
+          </Box>
+
+          <Box className='resource-sections' sx={{ wordWrap: 'break-word', marginBottom: 4 }}>
+            <Typography variant="h4">Substance Abuse</Typography>
+            <Link href='https://www.freerehabcenters.org/city/tx-houston' rel='noreferrer noopener' target='_blank' sx={{ display: 'block', marginBottom: 1 }}>
               Houston: Free Rehab Centers
-            </a>
-            <br />
-            <a
-              href='https://www.freerehabcenters.org/city/tx-austin'
-              rel='noreferrer noopener'
-              target='_blank'
-            >
+            </Link>
+            <Link href='https://www.freerehabcenters.org/city/tx-austin' rel='noreferrer noopener' target='_blank' sx={{ display: 'block', marginBottom: 1 }}>
               Austin: Free Rehab Centers
-            </a>
-            <br />
-          </div>
-          <div className='resource-sections'>
-            <h3>Mental Health</h3>
-            <a
-              href='https://publichealth.harriscountytx.gov/Resources/Mental-Health-Matters'
-              rel='noreferrer noopener'
-              target='_blank'
-            >
+            </Link>
+          </Box>
+
+          <Box className='resource-sections' sx={{ wordWrap: 'break-word', marginBottom: 4 }}>
+            <Typography variant="h4">Mental Health</Typography>
+            <Link href='https://publichealth.harriscountytx.gov/Resources/Mental-Health-Matters' rel='noreferrer noopener' target='_blank' sx={{ display: 'block', marginBottom: 1 }}>
               Houston: Mental Health Matters
-            </a>
-            <br />
-            <a
-              href='https://opencounseling.com/texas/austin'
-              rel='noreferrer noopener'
-              target='_blank'
-            >
+            </Link>
+            <Link href='https://opencounseling.com/texas/austin' rel='noreferrer noopener' target='_blank' sx={{ display: 'block', marginBottom: 1 }}>
               Austin: Open Counseling
-            </a>
-            <br />
-          </div>
-        </div>
-      </div>
+            </Link>
+          </Box>
+        </Box>
+      </Container>
     );
   }
 }
