206e327ed03fd32ba5d7b85583a4adcee580af16
xeviert <xevierturrubiartes@gmail.com>
2024-06-14 23:51:01 +0200

    home screenshot

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -118,76 +118,83 @@ const Home = () => {
   ];
 
   return (
-    <MDBContainer className={`py-5 h-100 mt-5 mb-4 ${isSmallScreen ? 'w-95' : 'w-60'}`} style={{ width: isSmallScreen ? '95%' : '60%' }}>
-      <MDBRow className="d-flex justify-content-center align-items-center">
-        <MDBCol xl="10">
-          <MDBCard style={{ borderRadius: "15px" }}>
-            <form
-              onSubmit={handleSubmit}
-              noValidate
-            >
-              <MDBCardBody className={isSmallScreen ? 'p-2' : 'p-5'}>
-                <h2 className="mb-4" style={{ color: theme.palette.primary.main, fontWeight: '800' }}>
-                  Wishlist
-                </h2>
-                <div className="d-flex justify-content-center align-items-center mb-4">
-                  <MDBInput
-                    type="text"
-                    id="form1"
-                    label="Location / Address:"
-                    wrapperClass="flex-fill"
-                    onChange={(e) => setLocationOnChange(e.target.value)}
-                    required
-                  />
-                </div>
+    <div style={{
+      backgroundColor: '#FBFCFE',
+      flexGrow: 1,
+      height: '100vh',
+      overflow: 'auto',
+    }}>
+      <MDBContainer className={`py-5 h-100 mt-5 mb-4 ${isSmallScreen ? 'w-95' : 'w-60'}`} style={{ width: isSmallScreen ? '95%' : '60%' }}>
+        <MDBRow className="d-flex justify-content-center align-items-center">
+          <MDBCol xl="10">
+            <MDBCard style={{ borderRadius: "15px" }}>
+              <form
+                onSubmit={handleSubmit}
+                noValidate
+              >
+                <MDBCardBody className={isSmallScreen ? 'p-2' : 'p-5'}>
+                  <h2 className="mb-4" style={{ color: theme.palette.primary.main, fontWeight: '800' }}>
+                    Wishlist
+                  </h2>
+                  <div className="d-flex justify-content-center align-items-center mb-4">
+                    <MDBInput
+                      type="text"
+                      id="form1"
+                      label="Location / Address:"
+                      wrapperClass="flex-fill"
+                      onChange={(e) => setLocationOnChange(e.target.value)}
+                      required
+                    />
+                  </div>
 
-                <MDBListGroup className="mb-0">
-                  {items.map((item) => (
-                    <MDBListGroupItem key={item.id} className="d-flex justify-content-between align-items-center border-start-0 border-top-0 border-end-0 border-bottom rounded-0 mb-2">
-                      <div className="d-flex align-items-center w-75">
-                        <div style={{ width: '30%' }}>
-                          <MDBCheckbox
-                            name="flexCheck"
-                            id="flexCheckChecked"
-                            className="me-3"
-                            checked={!!checkedItems[item.id]}
-                            onChange={() => toggleItemInList(item.id)}
-                            color="primary"
+                  <MDBListGroup className="mb-0">
+                    {items.map((item) => (
+                      <MDBListGroupItem key={item.id} className="d-flex justify-content-between align-items-center border-start-0 border-top-0 border-end-0 border-bottom rounded-0 mb-2">
+                        <div className="d-flex align-items-center w-75">
+                          <div style={{ width: '30%' }}>
+                            <MDBCheckbox
+                              name="flexCheck"
+                              id="flexCheckChecked"
+                              className="me-3"
+                              checked={!!checkedItems[item.id]}
+                              onChange={() => toggleItemInList(item.id)}
+                              color="primary"
+                            />
+                          </div>
+                          <div style={{ maxWidth: '400px', overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' }}>
+                            {item.name}
+                          </div>
+                        </div>
+
+                        <div style={{ width: '15%' }}>
+                          <MDBInput
+                            label="Qty"
+                            id="quantity"
+                            type="number"
+                            disabled={!checkedItems[item.id]}
+                            value={orderItems[item.id] || ""}
+                            onChange={(e) =>
+                              updateQuantityOfItem(item.id, e.target.value)
+                            }
+                            min={0}
+                            max={10}
+                            required
                           />
                         </div>
-                        <div style={{ maxWidth: '400px', overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' }}>
-                          {item.name}
-                        </div>
-                      </div>
+                      </MDBListGroupItem>
+                    ))}
+                  </MDBListGroup>
 
-                      <div style={{ width: '15%' }}>
-                        <MDBInput
-                          label="Qty"
-                          id="quantity"
-                          type="number"
-                          disabled={!checkedItems[item.id]}
-                          value={orderItems[item.id] || ""}
-                          onChange={(e) =>
-                            updateQuantityOfItem(item.id, e.target.value)
-                          }
-                          min={0}
-                          max={10}
-                          required
-                        />
-                      </div>
-                    </MDBListGroupItem>
-                  ))}
-                </MDBListGroup>
-
-                <MDBBtn type="submit" size="lg" className="ms-2 mt-4" style={{ backgroundColor: theme.palette.primary.main }}>
-                  Add
-                </MDBBtn>
-              </MDBCardBody>
-            </form>
-          </MDBCard>
-        </MDBCol>
-      </MDBRow>
-    </MDBContainer>
+                  <MDBBtn type="submit" size="lg" className="ms-2 mt-4" style={{ backgroundColor: theme.palette.primary.main }}>
+                    Add
+                  </MDBBtn>
+                </MDBCardBody>
+              </form>
+            </MDBCard>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
+    </div>
   );
 }
 
